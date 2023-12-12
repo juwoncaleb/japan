bcbfe109d531b179ede906d2d5c13cf70e22893e
JUWONCALEB <63285430+juwoncaleb@users.noreply.github.com>
2023-12-12 22:00:32 +0000

    HJ

--- components/Header.js
+++ components/Header.js
@@ -9,47 +9,57 @@ export default function Header() {
   };
   return (
     <div className="">
-      <div className=" flex justify-between desk">
+      <div className=" flex justify-center desk">
         <div className="flex justify-between">
-          <p className="header_element">MENU</p>
-          <p className="header_element">BOOKINGS</p>
-          <p className="header_element">FOR GUEST</p>
+          <Link href="./menu">
+            <p className="header_element"> MENU</p>
+          </Link>
+          <Link href="./booking">
+            <p className="header_element"> BOOKING </p>
+          </Link>
+          <Link href="./guest">
+            <p className="header_element"> GUEST</p>
+          </Link>
         </div>
-        <img className="logo" src="./lg5.png" />
+        <Link href="./">
+          <img className="logo" src="./lg5.png" />
+        </Link>
         <div className="flex justify-around">
-          <p className="header_element">OUR STORY</p>
+          <Link href="./story">
+            <p className="header_element">OUR STORY</p>
+          </Link>
           <p className="header_element">CONTACT-US</p>
           <p className="header_element">1</p>
         </div>
       </div>
-        <div className="flex mob mr-4 justify-between">
-          <img className="logo" src="./lg5.png" />
+      <div className="flex mob mr-4 justify-between">
+        <img className="logo" src="./lg5.png" />
 
-          <div className="mt-5">
-            {/* Hamburger Menu */}
-            {isMenuOpen === true ? (
-              <div onClick={toggleMenu}>
-                <img
+        <div className="mt-5">
+          {/* Hamburger Menu */}
+          {isMenuOpen === true ? (
+            <div onClick={toggleMenu}>
+              <img
                 className="close-icon"
-                  width="50"
-                  height="70"
-                  src="https://img.icons8.com/ios/50/multiply.png"
-                  alt="multiply"
-                />
-              </div>
-            ) : (
-              <div onClick={toggleMenu}>
-                <img
-                  width="30"
-                  height="50"
-                  src="https://img.icons8.com/ios/50/menu--v7.png"
-                  alt="menu--v7"
-                />
-              </div>
-            )}
-          </div>
+                width="50"
+                height="70"
+                src="https://img.icons8.com/ios/50/multiply.png"
+                alt="multiply"
+              />
+            </div>
+          ) : (
+            <div onClick={toggleMenu}>
+              <img
+                width="30"
+                height="50"
+                src="https://img.icons8.com/ios/50/menu--v7.png"
+                alt="menu--v7"
+              />
+            </div>
+          )}
         </div>
-        {isMenuOpen && (
+      </div>
+      {isMenuOpen && (
         <div className="text-right mr-4">
           <Link href="./">
             <p className="head_ele tag mt-10"> Home</p>
@@ -66,7 +76,6 @@ export default function Header() {
           <Link href="./guest">
             <p className="head_ele tag mt-10"> Guest</p>
           </Link>
-        
         </div>
       )}
     </div>
